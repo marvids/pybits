c56a05057f57cbfec98264bafa2ea6f9bcb9a9bd
Mikael Arvids <mikael.arvids@autoliv.com>
2014-09-22 15:00:35 +0200

    Add optional name

--- pyproto.py
+++ pyproto.py
@@ -1,42 +1,84 @@
 #!/usr/bin/python
 
+import json
 import collections
 from bitstring import ConstBitStream
 
 
+class Message(collections.OrderedDict):
+    def __init__(self, name=None, *args, **kwargs):
+        super(Message, self).__init__(*args, **kwargs)
+        self.name = name
+
+    def __str__(self):
+        s = ''
+        if self.name:
+            s += self.name + ' = '
+        return s + json.dumps(self, indent=4)
+
+
 class Token:
+    def __init__(self, *args, **kwargs):
+        args, kwargs = self.__handleOptionalName(args, kwargs)
+        self.init(*args, **kwargs)
+
+    def __handleOptionalName(self, args, kwargs):
+        if len(args) > 0 and isinstance(args[0], basestring):
+            self.name = args[0]
+            args = args[1:]
+        elif 'name' in kwargs:
+            self.name = kwargs['name']
+            del kwargs['name']
+        else:
+            self.name = None
+        return args, kwargs
+
+    def __call__(self, name):
+        self.name = name
+
+    def init(self):
+        pass
+
     def unserialize(self, data):
         return self.parse(ConstBitStream(data))
 
+
 class Sequence(Token):
-    def __init__(self, *args):
-        self.args = args
+    def init(self, *tokens):
+        self.tokens = tokens
 
     def parse(self, stream):
-        message = collections.OrderedDict()
-        for name, field in self.args:
-            value = field.parse(stream)
-            if name is not None:
-                message[name] = value
+        message = Message(self.name)
+        for token in self.tokens:
+            value = token.parse(stream)
+            if token.name:
+                message[token.name] = value
+            elif value:
+                message.update(value)
         return message
 
+
     def __add__(self, other):
-        args = self.args + other.args
-        return Sequence(*args)
+        tokens = self.tokens + other.tokens
+        return Sequence(*tokens)
 
 
 class Choice(Token):
-    def __init__(self, size, selector):
+    def init(self, size, selector):
         self.token = Bits(size)
         self.selector = selector
 
     def parse(self, stream):
-        value = self.token.parse(stream)
-        return self.selector[value].parse(stream)
+        select = self.token.parse(stream)
+        token = self.selector[select]
+        value = token.parse(stream)
+        if token.name:
+            return Message(self.name, {token.name: value})
+        return value
 
 
 class Repeat(Token):
-    def __init__(self, sequence):
+    def init(self, sequence):
         self.sequence = sequence
 
     def parse(self, stream):
@@ -47,7 +89,8 @@ class Repeat(Token):
 
 
 class Bits(Token):
-    def __init__(self, fmt, converter=None):
+    converter = None
+    def init(self, fmt, converter=None):
         if isinstance(fmt, int):
             self.fmt = str(fmt)
         else:
@@ -62,8 +105,16 @@ class Bits(Token):
             return val
 
 
-def Pad(size):
-    return (None, Bits(size))
+class Pad(Bits):
+    def init(self, size):
+        self.fmt = str(size)
+
+    def parse(self, stream):
+        Bits.parse(self, stream)
+
+
+class Integer(Bits):
+    pass
 
 
 class Unit:
@@ -72,4 +123,4 @@ class Unit:
     unit = ''
 
     def __str__(self):
-        return '{} {}'.format(self.factor*self + self.constant, self.unit)
+        return '\"{} {}\"'.format(self.factor*self + self.constant, self.unit)
--- test.py
+++ test.py
@@ -6,39 +6,44 @@ from pyproto import *
 
 class TestBitProtocolParser(unittest.TestCase):
     def testSequence(self):
-        msg = Sequence(('f1', Bits(4)))
+        msg = Sequence(Bits('f1', 4))
         self.unserialize(msg, '0x34', '{"f1": 3}')
 
     def testPad(self):
-        msg = Sequence(Pad(4), ('f1', Bits(4)))
+        msg = Sequence(Pad(4), Bits('f1', 4))
         self.unserialize(msg, '0xf8', '{"f1": 8}')
 
     def testChoice(self):
-        msg = Choice(4, {4: Sequence(('f1', Bits(4))), 5: Sequence(('f2', Bits(4)))})
+        msg = Choice(4, {4: Sequence(Bits('f1', 4)), 5: Sequence(Bits('f2', 4))})
         self.assertEqual(json.dumps(msg.unserialize('0x48')), '{"f1": 8}')
         self.assertEqual(json.dumps(msg.unserialize('0x52')), '{"f2": 2}')
 
     def testRepeat(self):
-        msg = Repeat(Sequence(('f1', Bits(4))))
+        msg = Repeat(Sequence(Bits('f1', 4)))
         self.assertEqual(json.dumps(msg.unserialize('0x48')), '[{"f1": 4}, {"f1": 8}]')
 
     def testConcatSequences(self):
-        msg1 = Sequence(('f1', Bits(4)))
-        msg2 = Sequence(('f2', Bits(4)))
+        msg1 = Sequence(Bits('f1', 4))
+        msg2 = Sequence(Bits('f2', 4))
         msg = msg1 + msg2
         self.assertEqual(json.dumps(msg.unserialize('0x87')), '{"f1": 8, "f2": 7}')
 
+    #def testEnumDict(self):
+    #    msg = Enum('flag', 1, {0: 'FALSE', 1: 'TRUE'})
+    #    self.unserialize(msg, '0x80', '{"flag": 'TRUE'}')
+    #    self.unserialize(msg, '0x00', '{"flag": 'FALSE'}')
+
+    #def testEnumTuple(self):
+    #    msg = Enum(2, ('FALSE', 'TRUE'), offset=1)
+
     def testComposite(self):
         msg = Sequence(
-                ('f1', Bits(8)),
+                Bits('f1', 8),
                 Pad(8),
-                ('f2', Sequence(
-                        ('g1', Bits(4))
-                    )),
-                ('f3', Repeat(
-                        Choice(4, {6: Sequence(('a1', Bits(8)), ('a2', Bits(8))),
-                                   7: Sequence(('a3', Bits(4)), ('a4', Bits(4)))})
-                    ))
+                Sequence('f2', Bits('g1', 4)),
+                Repeat('f3', Choice(4, {6: Sequence(Bits('a1', 8), Bits('a2', 8)),
+                                        7: Sequence(Bits('a3', 4), Bits('a4', 4))})
+                )
             )
         self.assertEqual(json.dumps(msg.unserialize('0x11ff265434726')), '{"f1": 17, "f2": {"g1": 2}, "f3": [{"a1": 84, "a2": 52}, {"a3": 2, "a4": 6}]}')
 
