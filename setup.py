74c09a6850e39a4d40a55678391fe1d05b1264a8
Mikael Arvids <mikael.arvids@autoliv.com>
2014-10-03 14:11:42 +0200

    Add setup

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(name='pybits',
+      version='0.1',
+      description='Parser for bit-encoded data',
+      author='Mikael Arvids',
+      author_email='mikael.arvids@gmail.com',
+      py_modules=['pybits'],
+  )
